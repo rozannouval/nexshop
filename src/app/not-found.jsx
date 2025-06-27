ef445c283ac8d7ca952783e8d4eb0115cdb63a67
rozannouval <rozannouval@gmail.com>
2025-06-27 22:57:28 +0700

    style(not-found): mengubah tinggi pada tampilan mobile dan dekstop

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -11,16 +11,20 @@ function NotFound() {
   };
 
   return (
-    <main className="flex flex-col justify-center items-center min-h-[80dvh] p-4">
-      <FileX className="size-32 md:size-48 text-stone-700" />
-      <h2 className="text-center font-bold text-2xl md:text-3xl my-4 md:my-8 text-stone-700">
+    <main className="container mx-auto p-4 md:p-8 min-h-[30rem] md:min-h-[75dvh] flex flex-col justify-center items-center gap-3 text-center">
+      <FileX className="size-32 md:size-40 text-stone-700" />
+      <h2 className="text-center font-bold text-xl md:text-3xl text-stone-700">
         Maaf, Halaman Tidak Ditemukan
       </h2>
+      <p className="font-medium text-sm md:text-lg text-stone-500">
+        Kami tidak dapat menemukan halaman yang anda tuju, mungkin halaman <br className="hidden md:block" />
+        tersebut sudah terhapus atau memang tidak ada
+      </p>
       <button
         onClick={handleBack}
-        className="bg-stone-700 text-white py-2 md:py-3 px-4 md:px-6 rounded-md hover:opacity-80 font-semibold text-lg md:text-xl cursor-pointer flex items-center justify-center gap-2"
+        className="bg-stone-700 text-white py-2 px-4 rounded-md hover:opacity-80 font-semibold text-sm md:text-lg cursor-pointer flex items-center justify-center gap-1"
       >
-        <ArrowLeft className="-ml-2" /> Kembali
+        <ArrowLeft className="-ml-1 size-4 md:size-5" /> Kembali
       </button>
     </main>
   );
